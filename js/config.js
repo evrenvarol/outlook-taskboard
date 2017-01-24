5536ac8dbbf7a54a822774bb33085b2381b07366
Reto Heer <reto.heer@gmail.com>
2017-01-24 08:49:57 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,8 +1,37 @@
 var tbConfig = angular.module('taskboardApp.config', []);
+
 var config_data = {
-  'GENERAL_CONFIG': {
-   // Outlook Task folders    // Name: Name of the task folder    // Title: Task lane title    // Limit: hard limits for each task lane. 0 = no limit    // Sort: Sort order for tasks (default = priority), can state multiple sort keys separated by comma, use '-' to sort descending, Example "duedate,-priority,subject"    // Restrict: Restrict certain tasks (default = only show incomplete tasks) (More info = https://msdn.microsoft.com/en-us/library/office/ff869597.aspx)    // Owner: If the task folder is shared by someone else, enter the name of the owner. (i.e. Evren Varol)    'BACKLOG_FOLDER':       { Name: '02_Backlog', Title: 'Backlog', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false", Owner: '' },    'INPROGRESS_FOLDER':  { Name: '04_InProgress', Title: 'Progress', Limit: 5, Sort: "-priority", Restrict: "[Complete] = false", Owner: ''},    'NEXT_FOLDER':    { Name: '03_Next', Title: 'Next', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},    'FOCUS_FOLDER':   { Name: '06_Testing', Title: 'Testing', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' },    'WAITING_FOLDER':   { Name: '05_Waiting', Title: 'Waiting', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false", Owner: '' },    'COMPLETED_FOLDER':     { Name: '07_Done', Title: 'Done', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' }, // Not Visible, only used to move when done    'ARCHIVE_FOLDER':       { Name: '08_Archive', Title: 'Done', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' },
-    // Task Note Excerpt Size    // number of chars for each task note    // 0 = makes no notes appear on the each task card    'TASKNOTE_EXCERPT':  100,
-    // Default task template    'TASK_TEMPLATE':        '\r\n\r\n### TODO:\r\n\r\n\r\n\r\n### STATUS:\r\n\r\n\r\n\r\n### ISSUES:\r\n\r\n\r\n\r\n### REFERENCE:\r\n\r\n\r\n\r\n'
-  }};
-angular.forEach(config_data,function(key,value) {  tbConfig.constant(value,key);});
+
+  'GENERAL_CONFIG': {
+
+  	// Outlook Task folders
+    // Name: Name of the task folder
+    // Title: Task lane title
+    // Limit: hard limits for each task lane. 0 = no limit
+    // Sort: Sort order for tasks (default = priority), can state multiple sort keys separated by comma, use '-' to sort descending, Example "duedate,-priority,subject"
+    // Restrict: Restrict certain tasks (default = only show incomplete tasks) (More info = https://msdn.microsoft.com/en-us/library/office/ff869597.aspx)
+    // Owner: If the task folder is shared by someone else, enter the name of the owner. (i.e. Evren Varol)
+    'BACKLOG_FOLDER':       { Name: '02_Backlog', Title: 'Backlog', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false", Owner: '' },
+    'INPROGRESS_FOLDER': 	{ Name: '04_InProgress', Title: 'Progress', Limit: 5, Sort: "-priority", Restrict: "[Complete] = false", Owner: ''},
+    'NEXT_FOLDER': 			{ Name: '03_Next', Title: 'Next', Limit: 0, Sort: "duedate,-priority", Restrict: "[Complete] = false", Owner: ''},
+    'FOCUS_FOLDER': 		{ Name: '06_Testing', Title: 'Testing', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' },
+    'WAITING_FOLDER': 		{ Name: '05_Waiting', Title: 'Waiting', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false", Owner: '' },
+    'COMPLETED_FOLDER':     { Name: '07_Done', Title: 'Done', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' },
+	// Not Visible, only used to move when done
+    'ARCHIVE_FOLDER':       { Name: '08_Archive', Title: 'Done', Limit: 0, Sort: "-priority", Restrict: "[Complete] = false or [Complete] = true", Owner: '' },
+
+    // Task Note Excerpt Size
+    // number of chars for each task note
+    // 0 = makes no notes appear on the each task card
+    'TASKNOTE_EXCERPT':		100,
+
+    // Default task template
+    'TASK_TEMPLATE':        '\r\n\r\n### TODO:\r\n\r\n\r\n\r\n### STATUS:\r\n\r\n\r\n\r\n### ISSUES:\r\n\r\n\r\n\r\n### REFERENCE:\r\n\r\n\r\n\r\n'
+
+  }
+};
+
+angular.forEach(config_data,function(key,value) {
+		tbConfig.constant(value,key);
+});
+
