f48d94c0a2272ac0581c0d1b53daac115f4c849b
Jan van Veldhuizen <jvv@onguard.com>
2017-08-03 22:57:33 +0200

    edited foler names

--- js/config.js
+++ js/config.js
@@ -10,11 +10,11 @@ var config_data = {
     // Limit: hard limits for each task lane. 0 = no limit
     // Sort: Sort order for tasks (default = priority), can state multiple sort keys separated by comma, use '-' to sort descending, Example "duedate,-priority,subject"
     // Restrict: Restrict certain tasks (default = only show incomplete tasks) (More info = https://msdn.microsoft.com/en-us/library/office/ff869597.aspx)
-    'BACKLOG_FOLDER':       { Name: '', Title: 'TODO', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'Not Started' And Not([Categories] = '!Next')"},
-    'INPROGRESS_FOLDER': 	{ Name: '', Title: 'IN PROGRESS', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'In Progress'"},
-    'NEXT_FOLDER': 			{ Name: '', Title: 'NEXT', Limit: 0, Sort: "duedate,-priority", Restrict: "[Categories] = '!Next' AND [Status] = 'Not Started'"},
-    'WAITING_FOLDER': 		{ Name: '', Title: 'WAITING', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'Waiting on someone else'"},
-    'COMPLETED_FOLDER':     { Name: '', Title: 'COMPLETED', Limit: 0, Sort: "-completeddate,-priority,subject", Restrict: "[Complete] = true "},
+    'BACKLOG_FOLDER':       { Name: '', Title: 'BACKLOG', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'Not Started'"},
+    'NEXT_FOLDER': 			{ Name: 'Kanban', Title: 'NEXT', Limit: 0, Sort: "duedate,-priority", Restrict: "[Status] = 'Not Started'"},
+    'INPROGRESS_FOLDER': 	{ Name: 'Kanban', Title: 'IN PROGRESS', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'In Progress'"},
+    'WAITING_FOLDER': 		{ Name: 'Kanban', Title: 'WAITING', Limit: 0, Sort: "-priority", Restrict: "[Status] = 'Waiting on someone else'"},
+    'COMPLETED_FOLDER':     { Name: 'Kanban', Title: 'COMPLETED', Limit: 0, Sort: "-completeddate,-priority,subject", Restrict: "[Complete] = true "},
 
     // Task Note Excerpt Size
     // number of chars for each task note
